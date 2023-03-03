7ac3d4c5409c9f929e15ce4e04a408daef64a0cf
karen cofre <karencofrecejas@gmail.com>
2023-03-03 12:47:34 -0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by karen cofré on 03-03-23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
